56ce38ba267dbb1ec5a50f785ae4e75e7dcd6c3e
Lukas Krickl <lukaskrickl@gmail.com>
2020-02-19 06:03:26 +0100

    Added time to crash output

--- restart.sh
+++ restart.sh
@@ -8,7 +8,7 @@ mkdir -p ./logs
 # run until exit code 0; redirect all output to log.txt
 until python3 Run.py >> "logs/$now.log" 2>> "logs/$now.error"; do
     # if not exit code 0 output error to shell and restart
-    echo "Bot crashed with exit code $?.  Respawning.." >&2
-    sleep 1
     now=$(date +"%H-%M-%S_%m_%d_%Y")
+    echo "$now: Bot crashed with exit code $?.  Respawning.." >&2
+    sleep 1
 done
