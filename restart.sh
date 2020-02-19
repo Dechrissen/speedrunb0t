b948047888a69fa6c907dcfff2da5c3e062fc85c
Lukas Krickl <lukaskrickl@gmail.com>
2020-02-19 06:00:35 +0100

    Added new logging script. Updated girignore acordingly.

--- restart.sh
+++ restart.sh
@@ -2,9 +2,13 @@
 
 echo "Running bot. Press ctrl+c to quit"
 
+now=$(date +"%H-%M-%S_%m_%d_%Y")
+mkdir -p ./logs
+
 # run until exit code 0; redirect all output to log.txt
-until python3 Run.py >> log.txt 2>> error.txt; do
+until python3 Run.py >> "logs/$now.log" 2>> "logs/$now.error"; do
     # if not exit code 0 output error to shell and restart
     echo "Bot crashed with exit code $?.  Respawning.." >&2
     sleep 1
+    now=$(date +"%H-%M-%S_%m_%d_%Y")
 done
